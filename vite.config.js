c1277092448cfd93a13bd930b731490de04fc86e
KushKK29 <kush282930@gmail.com>
2025-01-01 23:19:54 +0530

    add: last deploy

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Optional: Set the development server port
-  },
-  build: {
-    outDir: "dist", // Optional: Specify the output directory
-  },
-  resolve: {
-    alias: {
-      "@": "/src", // Optional: Set up path aliases for cleaner imports
-    },
-  },
   base: "/", // Ensure this is '/' for proper routing in production
 });
